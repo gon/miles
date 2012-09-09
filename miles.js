07cb0683d8b40de3f3046802e23e4706a9ae9ece
Jeremy <lubin.jeremy@gmail.com>
2012-09-09 10:18:08 -0700

    Final business view.

--- miles.js
+++ miles.js
@@ -6,13 +6,36 @@ $(document).ready(function() {
   $(".prospect").click( function() {expandProspect(this);} );
   $(".send").click( function() {sendMessage(this);} );
   
+  setInterval(function () {checkForMessage();}, 1000);
+  
 });
 
-function addSampleData (sampleCount)
+function checkForProspect ()
 {
+  
+//  $.get('http://jeremylubin.com/get_message.php', function (data) {receiveMessage(3, data);});
+  $.ajax({
+          url: 'http://miles.minute.im/get_message.php/?sender=consumer',
+          dataType: 'jsonp',
+          success:function (data) {receiveMessage(_prospects.length-1, data);}});
+}
+
+function addSampleData (sampleCount)
+{  
   for (var i = 0 ; i < sampleCount ; i++)
   {
-    receiveProspect(80, ["fashion", "food", "technology"], false);
+    if (i == 0) {
+      receiveProspect("@jugglerjer", "25", "male", 50, ["sports", "travel", "Lakers"], false);
+    }
+    else if (i == 1) {
+      receiveProspect("@awesomepete28", "40", "male", 64, ["technology", "coding", "Star Trek"], false);
+    }
+    else if (i == 2) {
+      receiveProspect("@planejanefromspain", "28", "female", 64, ["coffee", "architecture", "botany"], false);
+    }
+    else if (i == 3) {
+      receiveProspect("@gonzif", "30", "male", 80, ["fashion", "food", "technology"], false);
+    }
   }
 }
 
@@ -30,6 +53,9 @@ function openProspect (prospect)
 {
   $(prospect).children(".conversation").removeClass("hidden");
   $(prospect).addClass("open");
+  
+  $(".prospect").css("opacity", "0.5");
+  $(prospect).css("opacity", "1");
 }
 
 function closeProspect (prospect)
@@ -54,6 +80,25 @@ function sendMessage (button)
   
   // Clear the text field
   $(prospect).children(".conversation").children(".compose_box").children(".compose").val("");
+  
+  message = encodeURIComponent(message);
+  
+  //$.get('http://miles.minute.im/miles.php/?sender=business&message=' + message);
+  
+  $.ajax({
+            url: 'http://miles.minute.im/miles.php/?sender=business&message=' + message,
+            dataType: 'jsonp'});
+  
+}
+
+function checkForMessage ()
+{
+  
+//  $.get('http://jeremylubin.com/get_message.php', function (data) {receiveMessage(3, data);});
+  $.ajax({
+          url: 'http://miles.minute.im/get_message.php/?sender=consumer',
+          dataType: 'jsonp',
+          success:function (data) {receiveMessage(_prospects.length-1, data);}});
 }
 
 function receiveMessage (customerID, message)
@@ -68,13 +113,16 @@ function receiveMessage (customerID, message)
   $(prospect).children(".conversation").children(".chat").append("<div class='comment customer'>" + message + "</div>");
 }
 
-function receiveProspect (score, interests, live)
+function receiveProspect (name, age, gender, score, interests, live)
 {
   // Determine prospect rank
   // For now, it's rank #1
   var customerID = _prospects.length;
   
-  var newProspect = {'score': score,
+  var newProspect = {'name': name,
+                     'age': age,
+                     'gender': gender,  
+                     'score': score,
                      'interests': interests,
                      'customerID': customerID};
   _prospects.push(newProspect);
@@ -86,6 +134,7 @@ function displayProspect(newProspect, animated)
 {
   var HTML = "";
   HTML += "<div class='prospect' id='" + newProspect.customerID + "'>";
+  HTML +=   "<div class='name'><span class='name_text'><img class='name_icon' src='twitter.png' />" + newProspect.name + " | " + newProspect.age + " | " + newProspect.gender + "</span></div>";
   HTML +=   "<div class='match'><span class='score'>100%</span></div>";
   HTML +=   "<div class='influence'><span class='influence_text'><img class='influence_icon' src='influence.png' />" + newProspect.score + " Klout</span></div>";
   HTML +=   "<div class='interests'><span class='interest_text'><img class='interests_icon' src='interests.png' />Influential in ";
